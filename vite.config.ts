2d718e3025e9aec3842b2ed8eaec0cba59d350ea
Prabhat Patel <107596143+PRABHATPATEL01@users.noreply.github.com>
2026-03-24 15:54:40 +0530

    Fix: Set base to root for main portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/prabhat_patel/', // This must match your repo name exactly
+  base: '/', // This must match your repo name exactly
   plugins: [react()],
 })
